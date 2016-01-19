cebea316c5c2b5111af2fff8686cc5776be43dc8
John Bolitho <jbolitho@devjitsu.ninja>
2016-01-19 09:19:52 -0500

    Added auto generate data and faker

--- autogenerate.js
+++ autogenerate.js
@@ -1,15 +1,15 @@
 // See https://github.com/Marak/faker.js for API
-var faker = require('faker');
+var faker = require('faker')
 
-module.exports = function(){
-  var data = {users: []};
+module.exports = function () {
+  var data = {users: []}
 
-  for(var i = 0; i < 1000; i++){
+  for (var i = 0; i < 1000; i++) {
     data.users.push({
-      id:i,
+      id: i,
       info: faker.helpers.createCard()
-    });
+    })
   }
 
-  return data;
+  return data
 }
